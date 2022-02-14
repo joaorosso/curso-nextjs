ee6e3163b8a83792abc61e024aea66f857333759
João Rosso <joaorosso92@gmail.com>
2022-02-13 22:27:33 -0300

    create project

---
+++ pages/faq.js
@@ -0,0 +1,8 @@
+export default function FAQPage() {
+    return (
+        <div>
+            <h1>Curso ReactJS - FAQ</h1>
+            <a href="/">Home</a>
+        </div>
+    )
+}
---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function HomePage() {
+    return (
+        <div>
+            <h1>Curso ReactJS - Home</h1>
+            <a href="/faq">FAQ</a>
+        </div>
+    )
+}
