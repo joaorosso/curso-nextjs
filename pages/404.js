0b003dbc3b13c437492e6ba039cc7f46a5c78523
João Rosso <joaorosso92@gmail.com>
2022-02-14 21:16:58 -0300

    implement 404 page

---
+++ pages/404.js
@@ -0,0 +1,12 @@
+import Link from '../src/components/Link';
+
+export default function Page404() {
+    return (
+        <div>
+            <h2>Página não encontrada!</h2>
+            <Link href="/">
+                Home
+            </Link>
+        </div>
+    )
+}
