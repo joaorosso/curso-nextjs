8c0491f29fe80edc36f0dbc71d81e4de6a5e5e16
João Rosso <joaorosso92@gmail.com>
2022-02-18 20:15:51 -0300

    working with static files

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,3 @@
 import HomeScreen from "../src/screens/HomeScreen";
 
 export default HomeScreen;
-
-// import React from 'react';
-// import Link from '../src/components/Link';
-// import Footer from '../src/components/Footer';
-
-// function Title({ children, as }) {
-//     const Tag = as;
-//     return (
-//         <React.Fragment>
-//             <Tag>{ children }</Tag>
-//             <style jsx>{`
-//                 ${Tag} {
-//                     color: red;
-//                 }
-//             `}</style>
-//         </React.Fragment>
-//     )
-// }
-
-// export default function HomePage() {
-//     return (
-//         <div>
-//             <Title as="h1">Curso ReactJS - Home</Title>
-//             <Link href="/faq">
-//                 FAQ
-//             </Link>
-//             <Footer />
-//         </div>
-//     )
-// }
