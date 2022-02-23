c14d70b8c1269ea1c27f81b7a9c8bc1ebc799e5d
João Rosso <joaorosso92@gmail.com>
2022-02-22 21:37:37 -0300

    finish faq

--- pages/faq.js
+++ pages/faq.js
@@ -1,5 +1,6 @@
-import NextHead from 'next/head';
-import Link from '../src/components/Link';
+import FAQScreen from "../src/screens/FAQScreen";
+
+export default FAQScreen; 
 
 export async function getStaticProps() {
     const FAQ_API_URL = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json';
@@ -14,29 +15,4 @@ export async function getStaticProps() {
     return {
         props: { faq }
     };
-}
-
-export default function FAQPage({ faq }) {
-    return (
-        <div>
-            <NextHead>
-            <title>FAQ - Curso NextJS</title>
-            </NextHead>
-            <h1>Curso ReactJS - FAQ</h1>
-            <Link href="/">
-                Home
-            </Link>
-            <ul>
-                {faq.map(({ answer, question }) => (
-                    <li key={question}>
-                        <article>
-                            <h2>{question}</h2>
-                            <p>{answer}</p>
-                        
-                        </article>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    )
 }
